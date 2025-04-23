295bf55d695c3381641c5171560c09fa74b7af7a
Alejandro Salazar Lizardiz <asalazarlizardiz@gmail.com>
2025-04-23 16:19:37 -0400

    Commit for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:"https://crucelisspa.netlify.app/",
+  integrations: [react(), sitemap()],
+});
